4d4c0f20efcd61da7351307d34f5750634528564
mohamed Sayed <102462516+mohamedAbdelmaksoud22@users.noreply.github.com>
2023-01-25 01:58:47 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ValidatorModule
+
+A description of this package.
---
+++ Tests/ValidatorModuleTests/ValidatorModuleTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import ValidatorModule
+
+final class ValidatorModuleTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(ValidatorModule().text, "Hello, World!")
+    }
+}
