28938f7b90e3b409770143df5254cd66f2b77b94
mohamed Sayed <mrdiscover22@gmail.com>
2023-01-25 02:23:47 +0200

    add: Validator Package

--- Sources/ValidatorModule/ValidatorModule.swift
+++ Sources/ValidatorModule/ValidatorModule.swift
@@ -1,6 +1,68 @@
-public struct ValidatorModule {
-    public private(set) var text = "Hello, World!"
+import Foundation
 
-    public init() {
+class ValidatorModule {
+    
+    //Validate email address logic
+    public static func isValidMailInput(input: String) -> Bool {
+        let emailFormat = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailPredicate = NSPredicate(format:"SELF MATCHES %@", emailFormat)
+        return emailPredicate.evaluate(with: input)
+    }
+    
+    //Validate email address logic
+    public static func isValid(email: String) -> Bool {
+        //Declaring the rule of characters to be used. Applying rule to current state. Verifying the result.
+        let regex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+        let test = NSPredicate(format: "SELF MATCHES %@", regex)
+        let result = test.evaluate(with: email)
+        
+        return result
+    }
+    
+    //validate name logic
+    public static func isValid(name: String) -> Bool {
+        //Declaring the rule of characters to be used. Applying rule to current state. Verifying the result.
+        let regex = "[A-Za-z]{2,}"
+        let test = NSPredicate(format: "SELF MATCHES %@", regex)
+        let result = test.evaluate(with: name)
+        
+        return result
+    }
+    
+    //    length 6 to 16.
+    //    One Alphabet in Password.
+    //    One Special Character in Password.
+    public static func isValidPassword(password: String) -> Bool {
+        let passwordRegEx = "^(?=.*[a-z])(?=.*[$@$#!%*?&])[A-Za-z\\d$@$#!%*?&]{6,16}"
+        let passwordTest = NSPredicate(format:"SELF MATCHES %@", passwordRegEx)
+        let result = passwordTest.evaluate(with: password)
+        return result
+    }
+    
+    public static func isValidUrl(url: String) -> Bool {
+        let urlRegEx = "(http|https)://((\\w)*|([0-9]*)|([-|_])*)+([\\.|/]((\\w)*|([0-9]*)|([-|_])*))+"
+        let urlTest = NSPredicate(format:"SELF MATCHES %@", urlRegEx)
+        let result = urlTest.evaluate(with: url)
+        return result
+    }
+    
+    //==========================
+    //MARK:- PhoneNumber Validation
+    //==========================
+    public static func isValidPhoneNumber(_ PhoneNumber : String) -> Bool{
+        let PHONE_REGEX  = "^[0-9+]{0,1}+[0-9]{5,16}$"
+        let phoneTest = NSPredicate(format: "SELF MATCHES %@", PHONE_REGEX)
+        let result =  phoneTest.evaluate(with: PhoneNumber)
+        return result
+    }
+
+
+    //==========================
+    //MARK:- UserName Validation
+    //==========================
+    public static func isValidUsername(Username:String) -> Bool {
+        let RegEx = "\\A\\w{4,12}\\z"
+        let Test = NSPredicate(format:"SELF MATCHES %@", RegEx)
+        return Test.evaluate(with: Username)
     }
 }
--- Tests/ValidatorModuleTests/ValidatorModuleTests.swift
+++ Tests/ValidatorModuleTests/ValidatorModuleTests.swift
@@ -2,10 +2,140 @@ import XCTest
 @testable import ValidatorModule
 
 final class ValidatorModuleTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(ValidatorModule().text, "Hello, World!")
+    
+    
+    func testIsValidMailInput_WithValidData_ShouldReturnTrue() {
+        
+        //Act
+        let isVaildMail = ValidatorModule.isValidMailInput(input: "Soda@Soda.com")
+        
+        //Assert
+        XCTAssertTrue(isVaildMail)
+    }
+    
+    func testIsValidMailInput_WithInValidData_ShouldReturnFalse() {
+        
+        //Act
+        let isVaildMail = ValidatorModule.isValidMailInput(input: "Soda.com")
+        
+        //Assert
+        XCTAssertFalse(isVaildMail)
+    }
+    
+    func testIsValidEmail_WithValidData_ShouldReturnTrue() {
+        
+        //Act
+        let isVaildEmail = ValidatorModule.isValid(email: "Soda@Soda.com")
+        
+        //Assert
+        XCTAssertTrue(isVaildEmail)
+    }
+    
+    func testIsValidEmail_WithInValidData_ShouldReturnFalse() {
+        
+        //Act
+        let isVaildEmail = ValidatorModule.isValid(email: "Soda.com")
+        
+        //Assert
+        XCTAssertFalse(isVaildEmail)
+    }
+    
+    func testIsValidName_WithValidData_ShouldReturnTrue() {
+        
+        //Act
+        let isVaildName = ValidatorModule.isValid(name: "Soda")
+        
+        //Assert
+        XCTAssertTrue(isVaildName)
+    }
+    
+    func testIsValidName_WithInValidData_ShouldReturnFalse() {
+        
+        //Act
+        let isVaildName = ValidatorModule.isValid(name: "233Soda")
+        
+        //Assert
+        XCTAssertFalse(isVaildName)
+    }
+    
+    func testIsValidPass_WithValidPass_ShouldReturnTrue() {
+        
+        //Act
+        let isVaildpass = ValidatorModule.isValidPassword(password: "Soda#22A")
+        
+        //Assert
+        XCTAssertTrue(isVaildpass)
+    }
+    
+    func testIsValidPass_WithShortPass_ShouldReturnFalse() {
+        
+        //Act
+        let isVaildpass = ValidatorModule.isValidPassword(password: "S#22A")
+        
+        //Assert
+        XCTAssertFalse(isVaildpass)
+    }
+    
+    func testIsValidPass_WithLongtPass_ShouldReturnFalse() {
+        
+        //Act
+        let isVaildpass = ValidatorModule.isValidPassword(password: "S#22AADSw33333!fffvdvojdkdvkdpkv#Ddfv")
+        
+        //Assert
+        XCTAssertFalse(isVaildpass)
+    }
+    
+    func testIsValidURL_WithValidURL_ShouldReturnTrue() {
+        
+        //Act
+        let isVaildURL = ValidatorModule.isValidUrl(url: "https://www.google.com")
+        
+        //Assert
+        XCTAssertTrue(isVaildURL)
+    }
+    
+    func testIsValidURL_WithInValidURL_ShouldReturnFalse() {
+        
+        //Act
+        let isVaildURL = ValidatorModule.isValidUrl(url: "www.google.com")
+        
+        //Assert
+        XCTAssertFalse(isVaildURL)
+    }
+    
+    func testIsValidPhoneNumber_WithValidNumber_ShouldReturnTrue() {
+        
+        //Act
+        let isVaildPhone = ValidatorModule.isValidPhoneNumber("+20113287456")
+        
+        //Assert
+        XCTAssertTrue(isVaildPhone)
+    }
+    
+    func testIsValidPhoneNumber_WithInValidNumber_ShouldReturnFalse() {
+        
+        //Act
+        let isVaildPhone = ValidatorModule.isValidPhoneNumber("++20113287456")
+        
+        //Assert
+        XCTAssertFalse(isVaildPhone)
+    }
+    
+    func testIsValidUserName_WithValidName_ShouldReturnTrue() {
+        
+        //Act
+        let isVaildUserName = ValidatorModule.isValidUsername(Username: "MrSoda22")
+        
+        //Assert
+        XCTAssertTrue(isVaildUserName)
+    }
+    
+    func testIsValidUserName_WithInValidName_ShouldReturnFalse() {
+        
+        //Act
+        let isVaildUserName = ValidatorModule.isValidUsername(Username: "MRSODa3@@")
+        
+        //Assert
+        XCTAssertFalse(isVaildUserName)
     }
 }
